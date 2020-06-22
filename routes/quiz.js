443533a3c83beca744ef3c44980f7894207e696b
Karol Makara <karol_makara17@wp.pl>
2020-06-22 20:21:09 +0200

    nav

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/',(req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('quiz', { title: 'Quiz' });
 });
 
 module.exports = router;
